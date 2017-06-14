fee7755f2be74db09ddc7fddc8a2c82998349e14
Nicholas O'Donnell <nicholas@nicholasodonnell.com>
2017-06-14 10:12:25 -0400

    Increase version number to release @timkelty 's performance fix.

--- sidebarenhancer/SidebarEnhancerPlugin.php
+++ sidebarenhancer/SidebarEnhancerPlugin.php
@@ -20,7 +20,7 @@ class SidebarEnhancerPlugin extends BasePlugin
 
     public function getVersion()
     {
-        return '1.0.7';
+        return '1.0.8';
     }
 
     public function getReleaseFeedUrl()
